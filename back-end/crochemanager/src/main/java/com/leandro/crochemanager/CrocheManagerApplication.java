990d49f1d5a6d8511506f1450e851310eac0c745
leandro.ferreira <leandroferreiramine@gmail.com>
2025-04-28 10:32:09 -0300

    back-end

---
+++ back-end/crochemanager/src/main/java/com/leandro/crochemanager/CrocheManagerApplication.java
@@ -0,0 +1,13 @@
+package com.leandro.crochemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrocheManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrocheManagerApplication.class, args);
+	}
+
+}
---
+++ back-end/crochemanager/src/test/java/com/leandro/crochemanager/CrocheManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandro.crochemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrocheManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
